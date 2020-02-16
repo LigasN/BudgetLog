207d8f6cfb88549f06e17b1758d84d51b3fb0d0d
Norbert Ligas <norbert221198@gmail.com>
2020-02-16 22:22:02 +0100

    "Basic project set up"

---
+++ Windows/BudgetLog/BudgetLog.Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetLog.Common
+{
+    public class Class1
+    {
+    }
+}
